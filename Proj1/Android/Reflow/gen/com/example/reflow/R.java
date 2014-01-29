7996e6cf3134fc8a32e6429bf631050bbb37be74
esecules <esecules@gmail.com>
2014-01-28 21:41:04 -0800

    UI done except for strip chart

--- Proj1/Android/Reflow/gen/com/example/reflow/R.java
+++ Proj1/Android/Reflow/gen/com/example/reflow/R.java
@@ -24,12 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int editText1=0x7f080001;
-        public static final int editText2=0x7f080002;
+        public static final int action_settings=0x7f080008;
+        public static final int editText1=0x7f080000;
+        public static final int editText2=0x7f080006;
         public static final int editText3=0x7f080003;
-        public static final int progressBar1=0x7f080004;
-        public static final int toggleButton1=0x7f080000;
+        public static final int progressBar1=0x7f080002;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f080005;
+        public static final int textView3=0x7f080007;
+        public static final int toggleButton1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_graph_view=0x7f030000;
