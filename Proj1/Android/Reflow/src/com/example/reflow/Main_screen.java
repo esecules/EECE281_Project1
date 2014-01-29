65edb4796ce46c00844a0419b34a7b15e376de0a
esecules <esecules@gmail.com>
2014-01-28 20:17:43 -0800

    initial android commit

---
+++ Proj1/Android/Reflow/src/com/example/reflow/Main_screen.java
@@ -0,0 +1,23 @@
+package com.example.reflow;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Main_screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_screen, menu);
+        return true;
+    }
+    
+}
