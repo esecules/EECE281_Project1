e2d8c95e1bfafe6ba0ec11b68e20d0509b639512
vastopa <adamvastopa@gmail.com>
2014-02-05 17:45:38 -0800

    Added start/stop functionality
    Added start/stop functionality to key.1.
    Everything works, LCD implementation next.

--- Proj1/src/main.asm
+++ Proj1/src/main.asm
@@ -18,23 +18,49 @@ $include(decision.asm)
 $include(utilities.asm)
 $include(beeper.asm)
 
-	
+clearAll:
+	mov tempa+0, #0
+	mov tempa+1, #0
+	mov tempa+2, #0
+	mov tempo+0, #0
+	mov tempo+1, #0
+	mov tempi, #0
+	mov tempm+0, #0
+	mov tempm+1, #0
+	mov tempm+2, #0
+	mov time, #0
+	mov time+1, #0
+	mov timer2_interrupt_count, #0
+	mov heating_state, INITIAL
+	mov soak_temp, #0
+	mov soak_time, #0
+	mov max_temp, #0
+	mov dutycycle, #0
+	clr stateChange
+	clr run	
 MyProgram:
 	LCALL InitDE2
     LCALL InitSerialPort
 	LCALL Init_timer2
 	LCALL Init_SPI
 	LCALL InitSSR
-	setb run
+	clr run
 	mov heating_state, INITIAL
 	mov soak_temp, #50
 	mov soak_time, #30
 	mov reflow_temp, #120
 	mov reflow_time, #30
 	mov max_temp, #150
-	setb P0.7
-	
+	setb P0.7   
+
 Forever:
+	jb key.1, keep_going
+	jnb key.1, $
+	cpl run
+	jb run, keep_going
+	ljmp clearAll
+	
+	keep_going:
 	lcall Read335
 	lcall ReadThermo
 	lcall OFFSET
